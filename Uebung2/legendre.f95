5970f5871cf10cf5f331d9bf2c6e13a862fd60d1
autbass <ifroetscher@gmail.com>
2021-03-10 18:36:55 +0100

    legendre done

--- Uebung2/legendre.f95
+++ Uebung2/legendre.f95
@@ -1,8 +1,8 @@
 !Legendre polynome vom grad n berechnen. 
 !
+!Aufgabe 1
 module polinomials_module
     implicit none
-    
     contains
         double precision function legendre(n,x)
         integer :: n, k=0
@@ -41,10 +41,10 @@ module polinomials_module
         end function legendre_ableitung
 end module polinomials_module
 
-module newton_calc
+module calc
     use polinomials_module
     implicit none
-    
+    !Aufgabe 2
     contains
     subroutine legnewton(m,n,k,e,x0)
         integer :: m,n, k
@@ -52,30 +52,60 @@ module newton_calc
         n=0
         !abs(legendre(k,x0))>e .AND.
         do while (n<m .AND. abs(legendre(k,x0))>e)
-           ! print*, n, x0, legendre(k,x0)
-          x0=x0-legendre(k,x0)/(legendre_ableitung(k,x0))
-          n=n+1
+            ! print*, n, x0, legendre(k,x0)
+            x0=x0-legendre(k,x0)/(legendre_ableitung(k,x0))
+            n=n+1
         end do
     end subroutine legnewton
-end module newton_calc
+
+!Aufgabe 3
+    subroutine legintegrate(n,m,ergebnis, xmin, xmax, step, steps)
+        double precision, intent(out) :: ergebnis
+        double precision, intent(in) :: xmin, xmax, step
+        integer, intent(in)          :: n,m,steps
+        integer                      :: k=1
+        double precision             :: x=0
+        double precision, dimension(1:steps) :: poly
+        x=xmin
+        ergebnis=0
+        do k=1,steps-1
+            poly(k) = legendre(n,xmin+(k-1)*step)*legendre(m,xmin+(k-1)*step)
+        end do
+
+        ergebnis=ergebnis+poly(1)+poly(steps-1)
+        !integral=(step/2)*(f(n=1)+f(n=steps)+2*f(2<n<steps))
+        do k=2,(steps-3)
+            ergebnis=ergebnis+2*poly(k)
+            !write(*,*) k, ergebnis
+        end do
+        ergebnis=ergebnis*(step/(2.d0))
+        !write(*,*) "ergebnis subroutine, n, m", ergebnis,n,m
+    end subroutine legintegrate
+
+
+end module calc
 
 
 
 program legendre_main
     use polinomials_module
-    use newton_calc
+    use calc
     implicit none
-    integer :: n = 0, i=1, total = 0                            !Laufinteger. total benutzt um arrays anzup.f. unterschiedliche step
+    integer :: n = 0, m=0, i=1, total = 0                            !Laufinteger. total benutzt um arrays anzup.f. unterschiedliche step
     integer, parameter :: nmax = 10                             !Legendre Polynome bis grad N berechnen
     double precision, allocatable, dimension(:,:) :: pn         !2dim array für alle Legendre werte
     double precision, allocatable, dimension(:)   :: pn_Abl     !1dim array für Ableitung
-    double precision :: xmin=-1.d0, xmax=1.d0, x=0              !Grenzen, Laufindex
+    double precision, parameter :: xmin=-1.d0, xmax=1.d0              !Grenzen
+    double precision            :: x=0
     double precision, parameter :: step=.01                     !Schrittweite
     !newton Parameter
-    double precision, parameter :: abbruchbed = 1d-4            !abbruch bei:
+    double precision, parameter :: abbruchbed = .5d-3            !abbruch bei:
     integer, parameter          :: maxiterationen = 50          !schritte newtonverfahren
     integer, parameter          :: runs = 100000                !anzahl guesses startwerte
     double precision            :: guess
+
+    !integral result
+    double precision            :: integral
     
     x=xmin
     
@@ -112,7 +142,7 @@ program legendre_main
 
     !Write out legendre polinomials format: x, 0th, 1st, 2nd, 3rd ... with implicit do loop
     do while (x<xmax+step)
-        !write(1,*) x,(pn(n,i), n=0, nmax)
+        write(1,*) x,(pn(n,i), n=0, nmax)
         i=i+1
         x=x+step
     end do
@@ -127,7 +157,7 @@ program legendre_main
         i=i+1
     end do
     i=0
-    x=xmin
+    
     n=0
     !guess random no. 0-1, shift to -1<x<1, call newton, find roots. 
     do i=1,runs
@@ -137,6 +167,27 @@ program legendre_main
         if (n .NE. maxiterationen .AND. (guess>xmin) .AND. guess<xmax) write(2,*) guess, legendre(9,guess), "run/i", i, n
     end do
 
+    x=xmin
+    write(*,*) "Grad der legendrepolynome welche miteinand multipliziert+zw. -1,1 integriert werden sollen"
+    write(*,*) "2 ganze Zahlen eingeben"
+    read(*,*) n
+    read(*,*) m
+    call legintegrate(n,m, integral, xmin, xmax, step, total)
+    print*, "Produktintegral über legendre funktion grad n,m:"
+    if (abs(integral) <.01d0) then
+        write(*,*) "Ergebnis kleiner 10^-2, kompatibel mit 0"
+    else 
+        write(*,*) integral
+    end if
+    
+
+    !Prüfen ob routine funktioniert. Ja!
+    !do n=1,nmax
+    !    do m = 1, nmax
+    !    call legintegrate(n,m, integral, xmin, xmax, step, total)
+    !    if(integral >.01d0)write(*,*) n,m, integral
+    !    end do
+    !end do
 
     deallocate (pn)
     deallocate (pn_Abl)
--- plotfile.gp
+++
@@ -1,7 +0,0 @@
-set terminal pdf
-set output 'bsp2.pdf'
-set xlabel 'x'
-set ylabel 'y'
-plot 'result.dat' every 10 u 2:3 title "trajectory" w lp, 'result.dat' every 20 u 2:3:($4/5):($5/5) title "velocity"  w vectors 
-
-
