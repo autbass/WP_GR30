6a909cc6fa650e19cc08b702321e92babf8a92ee
autbass <ifroetscher@gmail.com>
2021-03-24 10:34:53 +0100

    rescaled to dBFS

--- Uebung4/abgabe/fft.f95
+++ Uebung4/abgabe/fft.f95
@@ -10,6 +10,7 @@ module filehandler
             integer ::ElementeimFile
             integer*2::sample
             double precision, dimension(arraysize) :: array
+            double precision :: dB_maxvalue=32768
             integer :: i=0,cnt
             integer:: ioerror=0
             open(1, file=filename, FORM='UNFORMATTED',access='direct', recl=2, iostat=ioerror)
@@ -17,7 +18,7 @@ module filehandler
             if (ioerror /= 0) print *, "Error opening input file:", filename
             do
                 read(1,rec=i,err=100) sample
-                array(i)=dble(sample)
+                array(i)=dble(sample)/dB_maxvalue
                 
                 ! write(*,*)i, array(i)
                 i=i+1
@@ -41,14 +42,18 @@ module filehandler
         subroutine print_fourier_to_file(out, arraysize, filename,freq_interval)
             integer:: arraysize, toelementN, i=1
             integer:: ioerror=0
+            double precision :: dB_maxvalue=32768
             double complex, dimension(arraysize/2+1) :: out
             character(len=250), intent(inout) :: filename
-            double precision :: freq_interval
+            double precision :: freq_interval, fact=1d0/(2d0*acos(-1.0d0))
+            
+            !Renormierung, factor 1/2pi drauf.
+            out=out*fact 
 
             open(2, file=filename, iostat=ioerror)
             if (ioerror /= 0) print *, "Error opening output file"
             do i=1,arraysize/2+1
-                write(2,*) freq_interval*i, ABS(out(i))*ABS(out(i))
+                write(2,*) freq_interval*i, 20d0*log10((ABS(out(i)))/dB_maxvalue) !20d0*log10((ABS(out(i)))/(dB_maxvalue))
            end do
         end subroutine
 
@@ -68,6 +73,7 @@ program main
     integer*2, parameter :: sample_rate = 22050 !1s, entspricht Hz
     Double precision :: Freq_Min=1d0/dble(sample_rate), Freq_max=11025d0, Dauer_file=1,freqIntervall=0
     double complex, dimension(arraysize/2+1) :: out =0d0
+    
 
     integer*8 :: fourier_plan=0
     call get_command_argument(1, filename)
@@ -107,5 +113,6 @@ program main
         /,'Vorhandene Elemente im File', I12,&
         /,'Dauer des Audios in Sekunden', F12.3,&
         /,'Sample_rate', I12,&
-        /,'Nyquist Frequenz', F12.0)
+        /,'Nyquist Frequenz', F12.0, &
+        /,'Outputfile ausgegeben in dBFS')
 end program
